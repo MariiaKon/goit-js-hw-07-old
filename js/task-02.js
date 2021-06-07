44e71782973856f716660174f25ee7fc5ba842a3
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2021-06-07 23:30:45 +0300

    add solution of the task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientsRef = document.getElementById('ingredients');
+
+for (let i = 0; i < ingredients.length; i++) {
+
+  const listItem = document.createElement('li');
+  listItem.textContent = `${ingredients[i]}`;
+ 
+  ingredientsRef.append(listItem);
+}
