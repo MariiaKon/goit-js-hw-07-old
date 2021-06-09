ec341fae8de63952da157811e2786db6b0e15d09
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2021-06-09 21:04:31 +0300

    Update task 6

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,13 @@ const inputEl = document.getElementById('validation-input');
 const inputLength = inputEl.getAttribute('data-length');
 
 inputEl.addEventListener('blur', onInputBlur)
-
-function onInputBlur(event) { 
-    inputEl.classList.add(inputEl.value.length == inputLength ? 'valid' : 'invalid')
+    
+function onInputBlur() {
+    if (inputLength == inputEl.value.length) {
+        inputEl.classList.remove('invalid');
+        inputEl.classList.add('valid');
+    } else {
+        inputEl.classList.remove('valid');
+        inputEl.classList.add('invalid');
+    }    
 }
