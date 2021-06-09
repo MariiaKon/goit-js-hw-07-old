38cc197d4dbe022e6f98e851a1d86ab04d66fa8d
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2021-06-09 21:45:54 +0300

    Add solution task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,23 +1,24 @@
-// Каждый созданный div:
-
-// Размеры самого первого div - 30px на 30px. Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
-
+const START_SIZE = 30;
 const boxDiv = document.getElementById('boxes');
 const input = document.querySelector('#controls input');
 const createBtn = document.querySelector('[data-action="render"]');
 const destroyBtn = document.querySelector('[data-action="destroy"]');
 
-createBtn.addEventListener('click', () => { createBoxes() });
-destroyBtn.addEventListener('click', () => { destroyBoxes() });
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
 
-function createBoxes(amount) {
-    amount = input.value;
-
+function createBoxes() {
+    const amount = input.value;
+    let sizeValue = START_SIZE;
+    
     for (let i = 0; i < amount; i++) {
         const divItem = document.createElement('div');
-        divItem.style.backgroundColor = `rgb(${getRandom(0, 255)}, ${getRandom(0, 255)}, ${getRandom(0, 255)})`
-        divItem.style.width = '30px';
-        divItem.style.height = '30px';
+
+        divItem.style.backgroundColor = `rgb(${getRandom(1, 255)}, ${getRandom(1, 255)}, ${getRandom(1, 255)})`
+        divItem.style.width = `${sizeValue}px`;
+        divItem.style.height = `${sizeValue}px`;
+
+        sizeValue += 10;
 
         boxDiv.appendChild(divItem);
     }
@@ -30,6 +31,7 @@ function getRandom(min, max){
 
 function destroyBoxes() {
     boxDiv.innerHTML = '';
+    sizeValue = START_SIZE;
 }
 
 
