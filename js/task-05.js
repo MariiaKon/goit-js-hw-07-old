9a8a8ad57cfe1511ed4fe44e953a3223063f4f37
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2021-06-10 19:44:48 +0300

    Update task 5

--- js/task-05.js
+++ js/task-05.js
@@ -4,8 +4,8 @@ const output = document.getElementById('name-output');
 input.addEventListener('input', onInputEvent)
 
 function onInputEvent (event) {
-    output.textContent = input.value;
     if (input.value.trim() == '') {
         output.textContent = 'незнакомец';
     }
+    output.textContent = input.value;
 }
